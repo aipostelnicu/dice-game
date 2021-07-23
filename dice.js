66369536ce2cca357d71aca49dd0f5c7fb6c283e
Adriana Postelnicu <adriana.postelnicu1@gmail.com>
2021-07-23 12:18:01 +0300

    Update dice.js

--- dice.js
+++ dice.js
@@ -16,12 +16,12 @@ let img2 = images[randomNumber2];
 img1 = document.getElementsByTagName("img")[1].setAttribute("src", img2);
 
 
-    if (randomNumber1 === randomNumber2) {
-        document.querySelector("h1").innerHTML = "Draw!";
-    } else if (randomNumber1 > randomNumber2) {
-        document.querySelector("h1").innerHTML = "Player 1 wins";
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player 1 wins!";
+    } else if (randomNumber1 < randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player 2 wins!";
     } else {
-        document.querySelector("h1").innerHTML = "Player 2 wins";
+        document.querySelector("h1").innerHTML = "Draw!";
     }
 }
 
